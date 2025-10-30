76e94ddf6011187448b6952a09f86a9103a0d5b6
Aqib Javaid <aqibjavaidabbasi@gmail.com>
2025-10-30 13:57:26 +0500

    fix: fixed routing issue in login navigate modal window

--- src/components/modals/LoginNavigateModal.tsx
+++ src/components/modals/LoginNavigateModal.tsx
@@ -4,15 +4,24 @@ import React from 'react'
 import Modal from '../custom/Modal'
 import Button from '../custom/Button'
 import { useTranslations } from 'next-intl'
+import { useRouter } from '@/i18n/navigation'
 
 function LoginNavigateModal({showModal, setShowModal}: {showModal: boolean, setShowModal: (showModal: boolean) => void}) {
     const t = useTranslations('Modals.LoginNavigate')
+    const router = useRouter()
     return (
         <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
             <div className='flex flex-col gap-4 items-center'>
                 <h2>{t('title')}</h2>
                 <p>{t('description')}</p>
-                <Button style="primary" onClick={() => setShowModal(false)} size="small">{t('loginCta')}</Button>
+                <Button 
+                    style="primary" 
+                    onClick={() => {
+                        router.push('/auth/login')
+                        setShowModal(false)
+                    }} 
+                    size="small">{t('loginCta')}
+                </Button>
             </div>
         </Modal>
     )
